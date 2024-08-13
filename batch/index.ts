a58b7d7c431d034a19b327e6eaf8440fd5868867
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-13 16:35:22 +0900

    add yml

--- batch/index.ts
+++ batch/index.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
 
-const supabase = createClient(process.env.VITE_SUPABASE_URL!, process.env.VITE_SUPABASE_ANON_KEY!);
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL!, import.meta.env.VITE_SUPABASE_ANON_KEY!);
 
 (async () => {
   // ユーザーテーブル削除

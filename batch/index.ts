1e733fa8f77e0122972f5b122c5a7fed69d5f784
Kei-dev-1213 <zz2003live@gmail.com>
2024-08-13 16:24:59 +0900

    batch追加

---
+++ batch/index.ts
@@ -0,0 +1,15 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const supabase = createClient(process.env.VITE_SUPABASE_URL!, process.env.VITE_SUPABASE_ANON_KEY!);
+
+(async () => {
+  // ユーザーテーブル削除
+  const usersDeleteResult = await supabase.from("users").delete().neq("user_id", 0);
+  // ユーザー技術テーブル削除
+  const userSkillDeleteResult = await supabase.from("user_skill").delete().neq("id", 0);
+  if (!usersDeleteResult.error && !userSkillDeleteResult.error) {
+    console.log("all delete successfully");
+  }
+})();
